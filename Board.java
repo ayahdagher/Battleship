1a2dbc3cbac4f88692b7b172fde8f9568cb1bb0f
Ayah Dagher <94849040+ayahdagher@users.noreply.github.com>
2022-03-10 14:50:31 +0000

    added code

--- Board.java
+++ Board.java
@@ -1,7 +1,9 @@
 public class Board {
 
-  private String[][] squares;
+  private String[][] squares = new String[10][10];
 
+// Milestone 1: Write the constructor code: you need to initialize the squares variable, and then set every cell in squares to "-" by using nested loops. Write the toString method, again using nested loops to print all values separated by spaces and with line breaks in appropriate positions. Test these methods by running the Battleship class (throughout this program you can type "b" to print the toString representation of the Board being used).
+  
   public Board() {
 
     for (int r = 0; r < squares.length; r++) {
@@ -15,7 +17,7 @@ public class Board {
 
     for (int r = 0; r < squares.length; r++) {
       for (int c = 0; c < squares[r].length; c++) {
-        System.out.println(squares[r][c]);
+        System.out.print(squares[r][c] + " ");
       }
 
       System.out.println(" ");
@@ -23,7 +25,25 @@ public class Board {
     return "";
   }
 
+// Milestone 2: Write the addShip method. It may be helpful to split this method into two parts: one where the parameter horizontal is true, the other where it is false. In each case you will need to use if statements to determine whether all parts of the ship are on the board, then iterate through the intended squares for the ship to determine whether there is already a ship in the way. If the ship can be placed you can iterate through the relevant squares again, changing them to "b". Don't forget to add returns to indicate what the outcome of placing the ship is. Test your method thoroughly using the runner class.
+
   public boolean addShip(int row, int col, int len, boolean horizontal) {
+    
+    if(horizontal == true)
+    {
+      
+    }
+    
+    if(horizontal == false)
+    {
+      
+    }
+    
+    
+    
+    
+    
+    
     return false;
   }
 
